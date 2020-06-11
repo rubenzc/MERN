9eda267599a861e2c5ea1ffa445ccabbef451363
rubenzc <rubenzc@me.com>
2020-06-11 08:58:36 +0200

    Added initial routing structure

--- personal-web/client/src/App.js
+++ personal-web/client/src/App.js
@@ -1,24 +1,19 @@
 import React from "react";
-import { DatePicker, Card } from "antd";
+import Admin from "./pages/Admin";
+import SignIn from "./pages/Admin/SignIn";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
 
 import "./App.scss";
 
 function App() {
-  const test = (date, dateString) => {
-    console.log(date, dateString);
-  };
-
   return (
-    <div className="app">
-      <h1>
-        Hello world! <span> Hi! </span>{" "}
-      </h1>
-      <h2> Test </h2>
-      <DatePicker onChange={test} />
-
-      <Card title="Default size card" style={{ width: 300 }}>
-        <p>Card content</p>
-      </Card>
+    <div>
+      <h1>App page</h1>
+      <Admin />
+      <SignIn />
+      <Home />
+      <Contact />
     </div>
   );
 }
